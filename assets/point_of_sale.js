9116351812dfa711d09caade723b017043565873
R Wiersema <rwiersema@gmail.com>
2022-04-12 12:02:37 +0200

    Commented out Combo specific sections

--- assets/point_of_sale.js
+++ assets/point_of_sale.js
@@ -95,48 +95,52 @@ function posOrder(name, modifier){
         // Display order total
         orderTable += "<tr><th colspan='3' class='ordertotal'>Total</th><th class='ordertotalprice'>$ " + orderTotal + "</th></tr>";
         // Create a total count of items to get
-        // Add header for total items
-        orderTable += "<tr><th colspan='4'>Itemlist</th></tr>";
-        // Copy orderObject to add items from menus to
-        const itemTotals = Object.assign({}, orderObject);
-        // For each item in itemTotals
-        for(tItem in itemTotals){
-            // For each group in the json array
-            for(tGroup in jsonData){
-                // Check if item is part of current group.
-                if(jsonData[tGroup].hasOwnProperty(tItem)){
-                    // Check if the current group is a combos group
-                    if(jsonData[tGroup].type == "combos"){
-                        // For each item that is part of the combo
-                        for(menuItem in jsonData[tGroup][tItem].items){
-                            // If item is already in object
-                            if(itemTotals.hasOwnProperty(menuItem)){
-                                // Add count to object
-                                itemTotals[menuItem] += (itemTotals[tItem] * jsonData[tGroup][tItem].items[menuItem]);
-                            }
-                            else{
-                                itemTotals[menuItem] = (itemTotals[tItem] * jsonData[tGroup][tItem].items[menuItem]);
-                            };
-                        };
-                        // Remove menu from list
-                        delete itemTotals[tItem];
-                    };
-                };
-            };
-        };
-        // For each item in itemTotals create a row
-        for(item in itemTotals){
-            for(group in jsonData){
-                if(jsonData[group].hasOwnProperty(item)){
-                    console.log("Menu item: " + item + " found in " + group);
-                    orderTable += "<tr>";
-                    orderTable += "<td class='ordercounter'>" + itemTotals[item] + "</td>";
-                    orderTable += "<td class='orderimage'><img src='products/"+ jsonData[group][item].image +"'/></td>";
-                    orderTable += "<td colspan='2'>" + item + "</td>";
-                    orderTable += "</tr>";
-                }
-            }
-        }
+
+        // START of combo specific section
+        // // Add header for total items
+        // orderTable += "<tr><th colspan='4'>Itemlist</th></tr>";
+        // // Copy orderObject to add items from menus to
+        // const itemTotals = Object.assign({}, orderObject);
+        // // For each item in itemTotals
+        // for(tItem in itemTotals){
+        //     // For each group in the json array
+        //     for(tGroup in jsonData){
+        //         // Check if item is part of current group.
+        //         if(jsonData[tGroup].hasOwnProperty(tItem)){
+        //             // Check if the current group is a combos group
+        //             if(jsonData[tGroup].type == "combos"){
+        //                 // For each item that is part of the combo
+        //                 for(menuItem in jsonData[tGroup][tItem].items){
+        //                     // If item is already in object
+        //                     if(itemTotals.hasOwnProperty(menuItem)){
+        //                         // Add count to object
+        //                         itemTotals[menuItem] += (itemTotals[tItem] * jsonData[tGroup][tItem].items[menuItem]);
+        //                     }
+        //                     else{
+        //                         itemTotals[menuItem] = (itemTotals[tItem] * jsonData[tGroup][tItem].items[menuItem]);
+        //                     };
+        //                 };
+        //                 // Remove menu from list
+        //                 delete itemTotals[tItem];
+        //             };
+        //         };
+        //     };
+        // };
+        // // For each item in itemTotals create a row
+        // for(item in itemTotals){
+        //     for(group in jsonData){
+        //         if(jsonData[group].hasOwnProperty(item)){
+        //             console.log("Menu item: " + item + " found in " + group);
+        //             orderTable += "<tr>";
+        //             orderTable += "<td class='ordercounter'>" + itemTotals[item] + "</td>";
+        //             orderTable += "<td class='orderimage'><img src='products/"+ jsonData[group][item].image +"'/></td>";
+        //             orderTable += "<td colspan='2'>" + item + "</td>";
+        //             orderTable += "</tr>";
+        //         }
+        //     }
+        // }
+        // END of combo specific section
+
         // Add reset button
         orderTable += "<tr><td colspan='4' id='reload'><button onClick='window.location.reload();'>Reset</button></td></tr>"
         // Close table
